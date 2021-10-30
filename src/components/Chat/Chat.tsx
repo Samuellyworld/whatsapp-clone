53da7e3e4eac4c351260418626a1efa54e847685
samuel <samuellyworld@gmail.com>
2021-10-30 06:15:20 -0700

    adding starting project

---
+++ src/components/Chat/Chat.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+
+
+const Chat = () => (
+    <h1> Chat </h1>
+)
+
+export default Chat;
---
+++ src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+
+
+const Sidebar =() => (
+    <h1> sidebar </h1>
+)
+
+export default Sidebar;
