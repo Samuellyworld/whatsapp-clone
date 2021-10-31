2a1750b1b159572065a7d2f952e3714b6b0381d7
samuel <samuellyworld@gmail.com>
2021-10-31 02:55:12 -0700

    adding styled-components

---
+++ src/App-Style.tsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const GlobalStyle = styled.div`
+ display : grid;
+ place-items: center;
+ height: 100vh;
+ background-color: #dadbd3;
+ 
+`
+
+export const MainContent  = styled.div`
+ display : flex;
+ background-color: #ededed;
+ margin-top: -50px;
+ height : 90vh;
+ width: 90vw;
+ box-shadow : -1px 4px 20px -6px rgba(0,0,0,0.75);
+`
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import * as  React from 'react';
-import './App.css';
+import {GlobalStyle, MainContent} from  './App-Style';
 import Chat from './components/Chat/Chat';
 import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
   return (
-    <div className="App">
-      <Sidebar/>
-      <Chat/>
-    </div>
+    <GlobalStyle>
+      <MainContent>
+       <Sidebar/>
+       <Chat/>
+      </MainContent>
+    </GlobalStyle>
   );
 }
 
