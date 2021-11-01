2bcd9255589700f6a3f8b880c4f6215099976fbb
samuel <samuellyworld@gmail.com>
2021-10-31 18:35:27 -0700

    fixing chat body background  🪐

--- src/components/Chat/Chat-Body/Chat-Body-Styles.tsx
+++ src/components/Chat/Chat-Body/Chat-Body-Styles.tsx
@@ -1,8 +1,15 @@
 import styled from 'styled-components';
+import img from '../../../assets/bg-chat-light.png';
 
 export const ChatBodyDiv = styled.div`
+    top: 0;
+    right: 0;
+    left: 0;
+    bottom: 0;
+    opacity: 0.05;
+    z-index: 1;
   flex : 1;
-  background: url('../../../assets/bg-chat-light.png') #E4DCD4;
+  background: url(${img})  #E4DCD4;
   background-repeat : repeat;
   background-position : center;
   padding : 38px;
