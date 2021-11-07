2c70ca62e69a1697498fe54e70ac9532e20e0df9
samuel <samuellyworld@gmail.com>
2021-11-07 04:49:38 -0800

    adding pusher 🔏

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-import * as  React from 'react';
+import  React,{useEffect} from 'react';
 import {AppStyles, MainContent} from  './App-Styles';
 import Chat from './components/Chat/Chat';
 import Sidebar from './components/Sidebar/Sidebar';
+import Pusher from 'pusher-js';
 
 function App() {
+
+  useEffect(() => {
+    const pusher = new Pusher('a01164abd7f41e43817a', {
+      cluster: 'us3'
+    });
+
+    const channel = pusher.subscribe('messages');
+    channel.bind('inserted', (data) => {
+      alert(JSON.stringify(data));
+    });
+  })
   return (
      <AppStyles>
       <MainContent>
