bb5df82fe3ad86c7a7be17b2b8f5856d8dcab2b7
samuel <samuellyworld@gmail.com>
2021-11-10 06:35:15 -0800

    destructuring and adding emoji picker ✨

--- src/components/Chat/Chat-Footer/Chat-Footer.tsx
+++ src/components/Chat/Chat-Footer/Chat-Footer.tsx
@@ -1,17 +1,38 @@
-import * as React from 'react';
+import React,{useState} from 'react';
 import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
-import ChatInput from './Chat-Input/Chat-Input';
 import MicIcon from '@material-ui/icons/Mic';
+import Picker from 'emoji-picker-react';
+import { IconButton } from '@material-ui/core';
 
-import { ChatFooterDiv } from './Chat-Styles';
+import { ChatFooterDiv, ChatInputForm } from './Chat-Styles';
 
-const ChatFooter = () => (
-  <ChatFooterDiv>
-      <InsertEmoticonIcon/>
-      <ChatInput/>
+const ChatFooter = () => {
+  const [text, setText] = useState("");
+  const [pickerVisible, togglePicker] = useState(false);
+
+  return ( <ChatFooterDiv>
+          {pickerVisible && (
+                <Picker
+                  pickerStyle={{ position: "absolute", bottom: "114px" }}
+                  onEmojiClick={(e, emoji) => {
+                      setText(text + emoji.emoji)
+                      togglePicker(false)
+                  }}
+                />
+              )}
+      <IconButton>        
+       <InsertEmoticonIcon onClick={() => togglePicker((pickerVisible) => !pickerVisible)}/>
+      </IconButton>
+      <ChatInputForm>
+        <input placeholder='Type a message' type='text'/>
+        <button type='submit'>
+            Send a message
+        </button>
+     </ChatInputForm>
       <MicIcon/>
   </ChatFooterDiv>
 
 );
+  }
 
 export default ChatFooter;
--- src/components/Chat/Chat-Footer/Chat-Styles.tsx
+++ src/components/Chat/Chat-Footer/Chat-Styles.tsx
@@ -13,3 +13,21 @@ export const ChatFooterDiv = styled.div`
   }
 
 `
+
+export const ChatInputForm = styled.form`
+  flex: 1;
+  display: flex;
+
+  > input {
+      flex : 1;
+      border-radius: 30px;
+      padding: 10px;
+      border: none;
+  }
+
+  > button {
+      display : none;
+  }
+ 
+
+`
