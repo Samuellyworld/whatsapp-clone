e32ccdb62282b87f16c72045d3c94ad38ed21349
samuel <samuellyworld@gmail.com>
2021-11-17 09:36:22 -0800

    adding login styles 🪐

--- src/components/Login/Login-Styles.tsx
+++ src/components/Login/Login-Styles.tsx
@@ -7,4 +7,30 @@ width: 100vw;
 display: grid;
 place-items: center;
 
+.login__container {
+    padding : 100px;
+    text-align: center;
+    background-color: white;
+    border-radius: 10px;
+    box-shadow : 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px  3px rgba(0, 0, 0, 0.12);
+
+    >img {
+        height: 100px;
+        object-fit: cover;
+        margin-bottom: 40px ;
+    }
+     >Button {
+        margin-top: 50px;
+        text-transform: inherit !important;
+        background-color: #0a8d48;
+        color: white;
+
+         img {
+            width: 20px;
+            height: 20px;
+            margin-left: 10px;;
+        }
+    }
+}
+
 ` 
