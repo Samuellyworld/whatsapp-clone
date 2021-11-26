9db3b11060021465a28ce042ca5c4fb3909cce96
samuel <samuellyworld@gmail.com>
2021-11-26 01:34:50 -0800

    adding dropdown styles ♨️

--- src/components/Option-Dropdown/Options-Styles.tsx
+++ src/components/Option-Dropdown/Options-Styles.tsx
@@ -5,23 +5,36 @@ export const OptionsDiv = styled.div`
     top: 78px;
     position: absolute;
     width: 130px;
-    height: 100px;
+    height: 135px;
     background-color: white;
     border-radius: 3px;
     box-shadow: 0 2px 5px 0 #00000042, 0 2px 10px 0 rgba(0, 0, 0, .16);  
     display: flex;
-    align-items: center;
-    justify-content: center;
+    align-items: left;
+    justify-content: left;
     flex-direction: column;
-    padding: 21px 0;
+    
 
-    li {
-    padding: 10px 0px;
-    margin-right: 16px;
-    list-style: none;
-    color: #000000;
-    font-size: 0.9rem;
-    cursor: pointer;
+    ul {
+     padding: 0 10px;
+     
+     
+        li {
+        padding: 10px 0px;
+        margin-right: 16px;
+        list-style: none;
+        color: #000000;
+        font-size: 0.9rem;
+        cursor: pointer;
+
+        &:hover {
+            background: #f5f5f5;
+        }
+        }
+
+
     }
+
+
 
     ` 
