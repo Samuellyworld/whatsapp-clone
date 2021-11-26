3d0645d0665bbb4932ddd63d6c84daace9ddb15e
samuel <samuellyworld@gmail.com>
2021-11-26 09:32:48 -0800

    implementing dark and light mode slider --building ��

--- src/components/Option-Dropdown/Options.tsx
+++ src/components/Option-Dropdown/Options.tsx
@@ -6,7 +6,14 @@ const Options = () => {
         <OptionsDiv>
          <ul>
              <li>Profile</li>
-             <li>Dark mode</li>
+             <li>
+                Dark mode
+                <label className='theme-switch'>
+                 <input type="checkbox" />
+                 <div className='slider round'></div>
+                </label> 
+
+             </li>
              <li> Log Out</li>
          </ul>
         </OptionsDiv>
