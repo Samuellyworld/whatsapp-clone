98d5dcc582437a7e3a26132b86566bb6bec233a1
samuel <samuellyworld@gmail.com>
2021-11-27 03:09:34 -0800

    adding light and dark toggle name --building 🥤

--- src/components/Option-Dropdown/Options.tsx
+++ src/components/Option-Dropdown/Options.tsx
@@ -2,22 +2,32 @@ import * as React from 'react';
 import {OptionsDiv} from './Options-Styles';
 
 const Options = () => {
+
+    const[mode, setMode] = React.useState("")
+    
  const switchTheme = (event) => {
     if(event.target.checked) {
             document.body.setAttribute('class', 'dark');
             localStorage.setItem('theme', 'dark');
+            setMode('Dark mode')
+
         } else {
             document.body.setAttribute('class', 'light');
             localStorage.setItem('theme', 'light')
+            setMode("Light Mode")
         }
     }
+
+    const getThemeNameOnLoad = localStorage.getItem('theme') 
+
+ 
 
     return (
         <OptionsDiv>
          <ul>
              <li>Profile</li>
              <li>
-                Dark mode
+                {  mode || getThemeNameOnLoad }
                 <label className='theme-switch'>
                  <input type="checkbox" onChange={switchTheme}/>
                  <div className='slider round'></div>
