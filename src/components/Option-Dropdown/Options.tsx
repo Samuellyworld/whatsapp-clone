9383e4ad45ed3f757ed82d9a6843f295c2135e76
samuel <samuellyworld@gmail.com>
2021-11-27 02:24:48 -0800

    adding dark and light mode --building 🌘

--- src/components/Option-Dropdown/Options.tsx
+++ src/components/Option-Dropdown/Options.tsx
@@ -2,6 +2,16 @@ import * as React from 'react';
 import {OptionsDiv} from './Options-Styles';
 
 const Options = () => {
+ const switchTheme = (event) => {
+    if(event.target.checked) {
+            document.body.setAttribute('class', 'dark');
+            localStorage.setItem('theme', 'dark');
+        } else {
+            document.body.setAttribute('class', 'light');
+            localStorage.setItem('theme', 'light')
+        }
+    }
+
     return (
         <OptionsDiv>
          <ul>
@@ -9,7 +19,7 @@ const Options = () => {
              <li>
                 Dark mode
                 <label className='theme-switch'>
-                 <input type="checkbox" />
+                 <input type="checkbox" onChange={switchTheme}/>
                  <div className='slider round'></div>
                 </label> 
 
