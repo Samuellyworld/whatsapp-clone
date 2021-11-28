29f631cc75599a8dec47a227ff90a829da45631d
samuel <samuellyworld@gmail.com>
2021-11-28 13:50:52 -0800

    alert --fixing anchor blank types check 🚦

--- src/components/Sidebar/Alert-Notification/Alert.tsx
+++ src/components/Sidebar/Alert-Notification/Alert.tsx
@@ -41,7 +41,7 @@ const alerts =  [
 				<a
 					className="underline"
 					href="https://faq.whatsapp.com/web/troubleshooting/cant-connect-to-whatsapp-web-or-desktop/"
-					target="_blank"
+
 				>
 					{" "}
 					Learn more.{" "}
